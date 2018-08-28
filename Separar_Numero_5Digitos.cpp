600d2b54f6073af98435326f02bfbd4ea95d0377
FLUBOR <42760498+FLUBOR@users.noreply.github.com>
2018-08-27 23:47:10 -0500

    Rename 2.cpp to Separar_Numero_5Digitos.cpp
    Escriba un programa que acepte como entrada un número de cinco dígitos, lo separe en sus distintos dígitos y
    los imprima separándolos cada uno con tres espacios. (Sugerencia: emplee los operadores de división y de
    módulo) Por ejemplo, si el usuario tecla 42339, el programa deberá imprimir:
    4 2 3 3 9

--- Separar_Numero_5Digitos.cpp
+++ Separar_Numero_5Digitos.cpp
@@ -1,6 +1,6 @@
-/*Escriba un programa que acepte como entrada un n˙mero de cinco dÌgitos, lo separe en sus distintos dÌgitos y
-los imprima separ·ndolos cada uno con tres espacios. (Sugerencia: emplee los operadores de divisiÛn y de
-mÛdulo) Por ejemplo, si el usuario tecla 42339, el programa deber· imprimir:
+/*Escriba un programa que acepte como entrada un n√∫mero de cinco d√≠gitos, lo separe en sus distintos d√≠gitos y
+los imprima separ√°ndolos cada uno con tres espacios. (Sugerencia: emplee los operadores de divisi√≥n y de
+m√≥dulo) Por ejemplo, si el usuario tecla 42339, el programa deber√° imprimir:
 4 2 3 3 9*/
 
 #include <iostream>
